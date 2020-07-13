efe4a8146bca9b9646725092bbeedeffa62cc494
Vijay Sharma <Sharmaa76Vijay@gmail.com>
2020-07-13 16:21:20 +0530

    Added Products controller

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,38 @@
+class ProductsController < ApplicationController
+  before_action :find_product, only: :show
+
+  def index
+    @products = current_user.products
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def show; end
+
+  def create
+    @product = current_user.products.new(product_params)
+    if @product.save
+      flash[:notice] = t('product.create.success')
+      redirect_to products_path
+    else
+      flash[:error] = t('errors.messages.not_saved')
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :title, :description, :sku, :product_type_id,
+      variants_attributes: [:sku, :price, :quantity, option_ids: []]
+    )
+  end
+
+  def find_product
+    @product = Product.find_by(id: params[:id])
+    redirect_to products_path unless @product
+  end
+end
