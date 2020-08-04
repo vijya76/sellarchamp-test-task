1ec21e40a9ce35ef5e37e6290f4072ed3c4fe33c
Vijay Sharma <Sharmaa76Vijay@gmail.com>
2020-08-04 15:37:44 +0530

    Improved rspec tests for products controller

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe ProductsController, type: :controller do
   let(:user) { FactoryBot.create(:user) }
   let(:product_type) { FactoryBot.create(:product_type) }
-  let(:option) { FactoryBot.create(:option) }
 
   before do
     @request.env['devise.mapping'] = Devise.mappings[:user]
@@ -25,9 +24,22 @@ RSpec.describe ProductsController, type: :controller do
       expect(flash[:notice]).to eq(I18n.t('product.create.success'))
       expect(product.product_type).to eq(product_type)
       expect(product.variants.count).to eq(1)
-      expect(product.variants.last.options.last).to eq(option)
 
       expect(response).to have_http_status(:redirect)
+    end
+
+    it 'should not create product with variants for duplicate options' do
+      expect { post :create, params: invalid_params }.to change { Product.count }.by(0)
+
+      expect(flash[:error]).to eq(I18n.t('errors.messages.not_saved'))
+      expect(response).to render_template :new
+    end
+
+    it 'should not create product with variants for dissimilar options' do
+      expect { post :create, params: dissimilar_option_params }.to change { Product.count }.by(0)
+
+      expect(flash[:error]).to eq(I18n.t('errors.messages.not_saved'))
+      expect(response).to render_template :new
     end
 
     it 'should not create product and variants for invalid params' do
@@ -40,7 +52,22 @@ RSpec.describe ProductsController, type: :controller do
   def params
     {
       product: { title: 'test', sku: 'test sku', product_type_id: product_type.id,
-      variants_attributes: [{ sku: 'test sku v', price: 10, quantity: 10, option_ids: [option.id] }] }
+      variants_attributes: [{ sku: 'test sku v', price: 10, quantity: 10, variant_options_attributes: [{option_type: 'color', option_value: 'red'}] }] }
+    }
+  end
+
+  def invalid_params
+    {
+      product: { title: 'test product', sku: 'UWX', product_type_id: product_type.id,
+      variants_attributes: [{ sku: 'KUI', price: 10, quantity: 10, variant_options_attributes: [{option_type: 'color', option_value: 'red'}, {option_type: 'color', option_value: 'red'}] }] }
+    }
+  end
+
+  def dissimilar_option_params
+    {
+      product: { title: 'test product', sku: 'UWX', product_type_id: product_type.id,
+      variants_attributes: [{ sku: 'KUI', price: 10, quantity: 10, variant_options_attributes: [{option_type: 'color', option_value: 'red'}, {option_type: 'size', option_value: 'L'}] },
+                            { sku: 'LVC', price: 90, quantity: 12, variant_options_attributes: [{option_type: 'color', option_value: 'red'}, {option_type: 'fabric', option_value: 'cotton'}] }]}
     }
   end
 end
