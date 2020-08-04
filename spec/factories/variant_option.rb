51861ec822fd66da83bd42140c28db6e5d7ea5b4
Vijay Sharma <Sharmaa76Vijay@gmail.com>
2020-08-04 15:21:05 +0530

    Improved rspec tests for variant option model

--- spec/factories/variant_option.rb
+++ spec/factories/variant_option.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :variant_option do
-    option
+    option_type { Faker::Name.name }
+    option_value { Faker::Name.name }
     variant
   end
 end
--- spec/models/variant_option_spec.rb
+++ spec/models/variant_option_spec.rb
@@ -5,12 +5,12 @@ require 'rails_helper'
 RSpec.describe VariantOption, type: :model do
   describe 'schema' do
     it { is_expected.to have_db_column(:variant_id).of_type(:integer) }
-    it { is_expected.to have_db_column(:option_id).of_type(:integer) }
+    it { is_expected.to have_db_column(:option_type).of_type(:string) }
+    it { is_expected.to have_db_column(:option_value).of_type(:string) }
   end
 
   describe 'associations' do
     it { is_expected.to belong_to(:variant) }
-    it { is_expected.to belong_to(:option) }
   end
 
   it 'has a valid factory' do
